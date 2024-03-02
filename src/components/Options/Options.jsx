2343c27adfe509d8e9859c8c655dad1f8c5bb34f
Granovskiy Uriy <silaveri777@gmail.net>
2024-03-02 17:20:10 +0200

    11

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,12 @@
-import css from "./Options.module.css";
+import css from './Options.module.css';
 
-const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
-
-  return (        
-  <ul className={css.optionsList}>
-    <button className={css.optionsBtn} onClick={() => updateFeedback('good')}>Good</button>
-    <button className={css.optionsBtn} onClick={() => updateFeedback  ('neutral')}>Neutral</button>
-    <button className={css.optionsBtn} onClick={() => updateFeedback('bad')}>Bad</button>
-    {totalFeedback !== 0 && (<button className={css.optionsBtn} onClick={resetFeedback}>Reset</button>)}      
-  </ul>  
-  )
-};
-
-export default Options;
+export default function Options({ option, total, reset }) {
+  return (
+    <div className={css.buttons}>
+      <button onClick={() => option('good')}>Good</button>
+      <button onClick={() => option('neutral')}>Neutral</button>
+      <button onClick={() => option('bad')}>Bad</button>
+      {total > 0 && <button onClick={reset}>Reset</button>}
+    </div>
+  );
+}
