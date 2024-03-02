68b35c83cf0a332e5c7bc2427a83f4fc4f8e2e34
Granovskiy Uriy <silaveri777@gmail.net>
2024-03-02 17:58:11 +0200

    14

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,67 +1,3 @@
-// import { useState } from 'react';
-// import { useEffect } from 'react';
-// import './App.css';
-// import Description from '../Description/Description';
-// import Options from '../Options/Options';
-// import Feedback from '../Feedback/Feedback';
-// import Notification from '../Notification/Notification';
-
-// export default function App() {
-//   const initialFeedback = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   const [feedback, setFeedback] = useState(() => {
-//     const savedfeedbacks = window.localStorage.getItem('feedbacks');
-//     return savedfeedbacks !== null
-//       ? JSON.parse(savedfeedbacks)
-//       : initialFeedback;
-//   });
-
-//   const updateFeedback = feedbackType => {
-//     setFeedback({
-//       ...feedback,
-//       [feedbackType]: feedback[feedbackType] + 1,
-//     });
-//   };
-
-//   useEffect(() => {
-//     window.localStorage.setItem('feedbacks', JSON.stringify(feedback));
-//   }, [feedback]);
-
-//   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-//   const positivePercent = Math.round(
-//     ((feedback.good + feedback.neutral) / totalFeedback) * 100
-//   );
-
-//   const resetFeedback = () => {
-//     setFeedback(initialFeedback);
-//   };
-
-//   return (
-//     <>
-//       <Description />
-//       <Options
-//         option={updateFeedback}
-//         total={totalFeedback}
-//         reset={resetFeedback}
-//       />
-
-//       {totalFeedback === 0 ? (
-//         <Notification />
-//       ) : (
-//         <Feedback
-//           value={feedback}
-//           total={totalFeedback}
-//           positive={positivePercent}
-//         />
-//       )}
-//     </>
-//   );
-// }
-
-
 import css from './App.module.css';
 import { useState, useEffect } from 'react';
 import Options from '../Options/Options';
@@ -69,31 +5,26 @@ import Feedback from '../Feedback/Feedback';
 import Notification from '../Notification/Notification';
 import Description from '../Description/Description';
 
-//object стану відгуків за замовченням
+
 const initialState = { good: 0, neutral: 0, bad: 0 };
 
-//ф-ція що зчитує значення localStorage за ключем
 const getInitialFeedback = () => {
   const storedFeedback = window.localStorage.getItem('feedback');
   return storedFeedback !== null ? JSON.parse(storedFeedback) : initialState;
 };
 
-//значення теккстових компонентів для можливості подальшого перевикористання
 const heading = 'Sip Happens Café';
 const paragraph =
   'Please leave your feedback on our service by selecting one of the options below.';
 const noFeedback = 'No feedback yet';
 
 const App = () => {
-  // зміна значення початкового стану
   const [feedback, setFeedback] = useState(getInitialFeedback);
 
-  // хук ф-ція збереження даних у localStorage
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(feedback));
   }, [feedback]);
 
-  //ф-ція зміни стану відгуків
   const updateFeedback = feedbackType => {
     setFeedback(prevFeedback => ({
       ...prevFeedback,
@@ -101,10 +32,8 @@ const App = () => {
     }));
   };
 
-  // Загальна кількість відгуків
   const caclTotalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  // Функція скидання відгуків
   const resetFeedback = () => {
     setFeedback(initialState);
   };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,24 +1,7 @@
-// export default function Feedback({ value, total, positive }) {
-//     return (
-//       <div>
-//         <p>Good: {value.good}</p>
-//         <p>Neutral: {value.neutral}</p>
-//         <p>Bad: {value.bad}</p>
-//         <p>Total: {total}</p>
-//         <p>Positive: {positive}%</p>
-//       </div>
-//     );
-//   }
-
-
 import css from './Feedback.module.css';
-//1. встановлюємо як пакет залежності проєкту бібліотеку prop-types
-//npm install--save - dev prop - types
-//2. імпорт бібліотеки
 import PropTypes from 'prop-types';
 
-// Підрахунок відсотка позитивних відгуків перенесено з App у компонент
-// Розрахунок positiveFeedback можна було б винести у окрему функцію для кращої зручності читання та потенційного повторного використання.
+
 const calcPositiveFeedback = (good, neutral, total) => {
   if (total === 0) {
     return 0;
@@ -45,9 +28,6 @@ const Feedback = ({ feedback, totalFeedback }) => {
   );
 };
 
-// додавання PropTypes для перевірки переданих пропсів компоненту з метою збільшення надійності та полегшення подальшої підтримки.
-// Опис типів пропсів компонента
-// shape— это валидатор PropTypes, который позволяет указать форму объекта, используется для более детального определения ожидаемой структуры свойства объекта.   shapeв валидатор принимает объект, где каждый ключ = имени свойства, а соответствующее значение является валидатором PropTypes для этого свойства.
 Feedback.propTypes = {
   feedback: PropTypes.shape({
     good: PropTypes.number.isRequired,
@@ -57,6 +37,4 @@ Feedback.propTypes = {
   totalFeedback: PropTypes.number.isRequired,
 };
 
-//згідно ТЗ для компонентів використовується експорт за замовчуванням (export default).
-//але використання іменованих експортів ( export const Feedback = ()=>{}; ) забезпечить кращу послідовність та передбачуваність при імпорті компоненту в іншому місці.
 export default Feedback;
